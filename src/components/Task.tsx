08e91e346c25c8a3b05bd1c29f4a1fb841f440b7
Vinicius <vinicius_auto@outlook.com>
2023-06-12 15:26:26 -0300

    changed module.css path import for vercel

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,5 @@
 import { Trash } from 'phosphor-react';
-import styles from './Task.module.css';
+import styles from "../components/task.module.css"
 
 interface taskProps {
   id: string,
@@ -9,29 +9,29 @@ interface taskProps {
   handleChangeisCompleted: (id: string) => void;
 }
 
-export function Task({id, isCompleted, content, handleDeleteTask, handleChangeisCompleted} : taskProps) {
+export function Task({ id, isCompleted, content, handleDeleteTask, handleChangeisCompleted }: taskProps) {
 
-  function onDeleteTask(id:string) {
+  function onDeleteTask(id: string) {
     handleDeleteTask(id);
   }
 
-  function onChangeisCompleted(id:string) {
+  function onChangeisCompleted(id: string) {
     handleChangeisCompleted(id);
   }
 
   return (
     <div className={styles.task}>
 
-      <input 
-        onClick={() => {onChangeisCompleted(id)}}
-        defaultChecked={isCompleted} 
-        type="checkbox" 
+      <input
+        onClick={() => { onChangeisCompleted(id) }}
+        defaultChecked={isCompleted}
+        type="checkbox"
         id={id}
       />
 
       <label htmlFor={id}>{content}</label>
 
-      <Trash onClick={() => {onDeleteTask(id)}} size={22}/>
+      <Trash onClick={() => { onDeleteTask(id) }} size={22} />
 
     </div>
   )
